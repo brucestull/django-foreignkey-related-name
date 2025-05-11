df2d18a860e2680779f308380dd07a90d5f15142
FlynntKnapp <bruce.stull@gmail.com>
2025-05-11 19:48:37 -0400

    In progress

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="notes/")),
     path("notes/", include("notes.urls")),
 ]
